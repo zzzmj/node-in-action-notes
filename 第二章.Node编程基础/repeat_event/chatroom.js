37f334c134942e102807385ee8b90894183c8c7e
zmj <18270632198@163.com>
2019-03-09 18:59:40 +0800

    chatroom

--- 第二章.Node编程基础/repeat_event/chatroom.js
+++ 第二章.Node编程基础/repeat_event/chatroom.js
@@ -1,36 +1,27 @@
 const events = require('events')
 const net = require('net')
 
-class Channel extends events.EventEmitter {
-    constructor() {
-        super()
-        this.clients = {}
-        this.subscriptions = {}
+const channel = new events.EventEmitter()
+channel.clients = {}
+channel.subscriptions = {}
+channel.on('join', function(id, client) {
+    console.log(`监听到${id}号用户加入聊天室`)
+    this.clients[id] = client
+    this.subscriptions[id] = (senderId, msg) => {
+        // 忽略发送广播消息的用户
+        if (id != senderId) {
+            this.clients[id].write(msg)
+            console.log(`${id}号用户收到广播消息${msg}`)
+        }
     }
+    this.on('broadcast', this.subscriptions[id])
+})
 
-    start() {
-        this.on('join', function(id, client) {
-            console.log(`监听到${id}号用户加入聊天室`)
-            this.clients[id] = client
-            this.subscriptions[id] = (senderId, msg) => {
-                // 忽略发送广播消息的用户
-                if (id != senderId) {
-                    this.clients[id].write(msg)
-                    console.log(`${id}号用户收到广播消息${msg}`)
-                }
-            }
-            this.on('broadcast', this.subscriptions[id])
-        })
+channel.on('leave', function(id) {
+    console.log(`${id}号用户已经退出直播间`)
+    channel.removeListener('broadcast', this.subscriptions[id])
+})
 
-        this.on('leave', function(id) {
-            console.log(`${id}号用户已经退出房间`)
-            channel.removeListener('broadcast', this.subscriptions[id])
-        })
-    }
-}
-
-const channel = new Channel()
-channel.start()
 let id = 0
 const server = net.createServer((client) => {
     const user = ++id
@@ -44,5 +35,7 @@ const server = net.createServer((client) => {
         channel.emit('leave', user)
     })
 })
+
 server.listen(8888)
+
 // telnet 127.0.0.1 8888
